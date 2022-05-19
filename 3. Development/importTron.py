e09e236a302272ad1ca6c7a45f2dce36a6dbccbe
andresovichh <ahenderson.holberton@gmail.com>
2022-05-19 10:49:17 -0300

    documented module

--- 3. Development/importTron.py
+++ 3. Development/importTron.py
@@ -1,4 +1,7 @@
-
+"""
+This is a module that imports information from the tronscan API.
+More information regarding how the api is used can be found here:
+https://developers.tron.network/v4.0/reference/trc20-transaction-information-by-account-address"""
 import requests
 import json
 import pprint
