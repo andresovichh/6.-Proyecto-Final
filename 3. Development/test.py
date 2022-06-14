42bfcc12c0c7c66fa549c684c40de0eb5d96086e
andresovichh <ahenderson.holberton@gmail.com>
2022-06-14 16:37:34 +0000

    test

--- 3. Development/test.py
+++ 3. Development/test.py
@@ -1,9 +1,14 @@
 
 
-value = 27082468
+value = 500000001
 str_value = str(value)
 str_len = len(str_value)
 decimals = 6
+f_value = str_value[:(str_len - decimals)]+ '.' + str_value[(str_len - decimals):]
+print(type(f_value))
+print(f_value)
+print(float(f_value))
+print(type(float(f_value)))
 
 print(len(str_value))
-print(str_value[:(str_len - decimals)]+ '.' + str_value[(str_len - decimals):])
+print()
