65638bd3756a12ca3d56868296d58fd9d2a5e5b6
andresh <ahenderson.holberton@gmail.com>
2022-06-21 11:26:59 -0300

    Diego, arreglá posición QR

--- 9.testjs/CryptoGateway_WEB/get_pump_name.js
+++ 9.testjs/CryptoGateway_WEB/get_pump_name.js
@@ -16,18 +16,55 @@ fetch(api_url, requestOptions).then((data)=>{
   // here I am getting the name of each one of the pumps
   // that the user can see
   // console.log(completedata);
-  var needle = 'pump 2';
+  // Here we get the name of the user-selected pump
+  var needle = selected_pump;
 // This matches a pump name with a wallet_id
 // iterate over each element in the array
 for (var i = 0; i < completedata.length; i++){
   // look for the entry with a matching `code` value
   if (completedata[i].name == needle){
-     // we found it
-    console.log(completedata[i].wallet_id)
+     /* 
+     we found that the pump name matches
+     the user-selected pump,
+     therefore we can get the wallet_id. 
+     And we can use this wallet_id
+     to console log it and then we will be 
+     able to pass it to the next page.
+
+     Instead of wallet_id we can use any of
+     the parameters that are returned in the
+     JSON from the API we are pinging.
+
+     */
+
+    var wallet_id = completedata[i].wallet_id;
+    console.log(wallet_id);
+    sessionStorage.setItem('wallet_id', wallet_id);
   }
 }
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function getCountryByCode(code) {
 //     return completedata.filter(
