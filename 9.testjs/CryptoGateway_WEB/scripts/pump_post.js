1c305cfeca94410506a30055ecbd4db3f7e93ebf
andresh <ahenderson.holberton@gmail.com>
2022-06-21 16:47:48 -0300

    some changes

---
+++ 9.testjs/CryptoGateway_WEB/scripts/pump_post.js
@@ -0,0 +1,24 @@
+/*
+This function is intended to send information to the
+API that will be used to check if the TX goes through or not.
+*/
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Basic YW5kcmVzOmFuZHJlcw==");
+myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+var urlencoded = new URLSearchParams();
+urlencoded.append("pump_id", "pump_id_value");
+urlencoded.append("cripto_id", "wallet_address");
+urlencoded.append("amount_uy", "peso_amount");
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: urlencoded,
+  redirect: 'follow'
+};
+
+fetch("http://44.204.28.37:8000/api/pump/", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
