7c6c36e659d5745be6d367b1a61e2a53986c0733
andresh <ahenderson.holberton@gmail.com>
2022-06-27 18:53:35 -0300

    changes

--- 9.testjs/CryptoGateway_WEB/scripts/check_and_store_tx.js
+++ 9.testjs/CryptoGateway_WEB/scripts/check_and_store_tx.js
@@ -8,6 +8,7 @@ function check_and_store_tx() {
     Then, we have to sort the array by the date in order to get
     the last Tx id for that pump.
     */
+    var last_tx_arr = new Array();
     var res;
     const api_url = "https://www.andreshenderson.tech/api/tran/"
     var myHeaders = new Headers();
@@ -61,14 +62,77 @@ function check_and_store_tx() {
     //console.log(txs[0]);
     return txs[0];
 })
-.then((last_tx) => {
-    // last_tx is the last Tx that has been stored in our database
-    console.log(last_tx)})
+// .then((last_tx) => {
+//     // last_tx is the last Tx that has been stored in our database
+//     console.log(last_tx)})
     .then((last_tx) =>{
+        console.log(last_tx);
+        last_tx_arr.push(last_tx);
         // ---------------------------------------------------------
+        var interval = setInterval(function (){
+            var requestOptions = {
+                method: 'GET',
+                redirect: 'follow'
+              };
+              console.log("THIS IS THE LAST" + last_tx_arr[0].cripto_id);
+              fetch("https://api.trongrid.io/v1/accounts/TYvYRiuVU6B4dQKTuyhvxxFsJKiJZiTCfL/transactions/trc20?only_confirmed=true&limit=1", requestOptions)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(response => {
+                    //if (last_tx == response.data[0].transaction_id)
+                    //console.log(response.data[0].transaction_id);
+                    //clearInterval(interval);
+                    console.log(last_tx)
+                    // if we get a tx id from tron, it means there is a tx
+                    if (response.data[0].transaction_id)
+                    {
+                        // if our last recorded cripto_id is same as the
+                        // last data to come from tron, it means that TX
+                        // hasn't changed
+                        if (response.data[0].transaction_id == last_tx.cripto_id)
+                        {
+                            console.log("Transaction hasn't changed");
+                            // clearInterval(interval);
+                        }
+                        /*
+                        If the transaction_id is different from the last 
+                        cripto_id we have recorded, it means that there
+                        hase been a new transaction recorded at Tron.
+                        So we can check:
+                        */
+                        else
+                        {
+                        console.log("Transaction HAS changed :" + response.data[0].transaction_id);
+                        // clearInterval(interval);
+                        // check if the Tx value is same as ours
+                            if (response.data[0].value == "1000000")
+                            {
+                                console.log("Transaction value is correct");
+                                window.location.replace("")
+                                clearInterval(interval);
 
-
-
+                            }
+                            // Tx value is incorrect, therefore unsuccessful
+                            else{
+                                console.log("Transaction value is incorrect");
+                                //clearInterval(interval);
+                            }
+                        }
+                    }
+                    else {
+                        console.log("Transaction hasn't changed");
+                        // clearInterval(interval);
+                    }
+                // if (last_tx == response.json()[0].id) {
+                //     console.log("Transaction has been confirmed");
+                // }
+                })
+        }, 1500);
+        // Run for at most, 120000 miliseconds, equal to 2 minutes
+        setTimeout(function( ) { clearInterval( interval ); }, 6000);
+        
+    
         //----------------------------------------------------------
 
     });
